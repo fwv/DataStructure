3cf3ed86db03f19db7feb173e5a2caf158517b0c
wei feng <1030557033@qq.com>
2016-08-22 13:59:58 +0800

    2016/08/22

--- src/main/java/com/fw/leetCode/BinaryTreeRightSide.java
+++ src/main/java/com/fw/leetCode/BinaryTreeRightSide.java
@@ -12,92 +12,75 @@ import java.util.logging.Logger;
 public class BinaryTreeRightSide {
     public static Logger log = Logger.getLogger(BinaryTreeRightSide.class.getName());
     public BinaryTree tree;
-    private List<Node> parents = new ArrayList<Node>();
+    private LinkedList<Node> p = new LinkedList<Node>();
+    private LinkedList<Node> gp = new LinkedList<Node>();
 
     public BinaryTreeRightSide() {
         tree = new BinaryTree();
     }
 
-    // 获取二叉树右视角能够看到的元素
     //              1            <---
     //            /   \
     //           2     3         <---
     //            \     \
-    //             5     4       <---
-    //            return [1, 3, 4]
+    //             5     x       <---
+    //            return [1, 3, 5]
     public List<Integer> rightSideView(Node root) {
         LinkedList<Integer> result = new LinkedList<Integer>();
         Queue<Node> queue = new LinkedList<Node>();
-        LinkedList<Node> temp = new LinkedList<Node>();
+        Node lastNode = root;
         if(null != root) {
             queue.add(root);
             result.add(root.iData);
-            parents.add(root);
+            gp.add(root);
+            if (null == root.leftChild && null == root.rightChild)return result;
             while(!queue.isEmpty()) {
                 Node cur = queue.poll();
+                lastNode = cur;
                 if(cur.leftChild != null) {
-                    if(isFirst(cur.leftChild)) {
-                        parents.clear();
-                        parents = temp;
-                        parents.add(cur);
-                        if(!temp.isEmpty()) {
-                            Integer n = temp.getLast().iData;
-                            result.add(n);
-                        }
-                        temp.clear();
+                    if (!isBelong(cur.leftChild, gp)) {
+                        Integer n = p.getLast().iData;
+                        result.add(n);
+                        gp = (LinkedList<Node>) p.clone();
+                        p.clear();
                     }
+                    p.add(cur.leftChild);
                     queue.add(cur.leftChild);
-                    temp.add(cur.leftChild);
                 }
                 if(cur.rightChild != null) {
-                    if(isFirst(cur.leftChild)) {
-                        parents.clear();
-                        parents = temp;
-                        parents.add(cur);
-                        if(!temp.isEmpty()) {
-                            Integer n = temp.getLast().iData;
-                            result.add(n);
-                        }
-                        temp.clear();
+                    if (!isBelong(cur.rightChild, gp)) {
+                        Integer n = p.getLast().iData;
+                        result.add(n);
+                        gp = (LinkedList<Node>) p.clone();
+                        p.clear();
                     }
+                    p.add(cur.rightChild);
                     queue.add(cur.rightChild);
-                    temp.add(cur.rightChild);
                 }
             }
+            result.add(lastNode.iData);
         }
         return result;
     }
 
-    public boolean isFirst(Node node) {
-        if (parents.isEmpty())return true;
-        for(Node parent : parents) {
+    public boolean isBelong(Node node, List<Node> list) {
+        if (list.isEmpty())return false;
+        for(Node parent : list) {
             if (parent.leftChild == node || parent.rightChild == node) {
-                return false;
+                return true;
             }
         }
-        return true;
-    }
-
-    public int depth(Node node) {
-        return tree.getMaxDepth(node);
-    }
-
-    public void deleteFlag(List<Integer> list) {
-        Iterator<Integer> it = list.iterator();
-        while(it.hasNext()) {
-            if(-1 == it.next())
-                it.remove();
-        }
+        return false;
     }
 
     public static void main(String[] args) {
         BinaryTreeRightSide ts = new BinaryTreeRightSide();
-        ts.tree.insert(6);
-        ts.tree.insert(1);
-        ts.tree.insert(3);
-        ts.tree.insert(2);
         ts.tree.insert(5);
-        ts.tree.insert(4);
+        ts.tree.insert(2);
+        ts.tree.insert(7);
+        ts.tree.insert(3);
+        ts.tree.insert(6);
+
         ts.tree.frontOrder(ts.tree.root);
         List<Integer> list = ts.rightSideView(ts.tree.root);
         log.info("============== RIGHT SIDE ==============");
