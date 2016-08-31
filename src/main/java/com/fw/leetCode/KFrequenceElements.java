a0f6ddb137263904dfa5c051e6ae540bf1356001
wei feng <1030557033@qq.com>
2016-08-31 16:48:51 +0800

    Leetcode 347. K Frequence Elements

--- src/main/java/com/fw/leetCode/KFrequenceElements.java
+++ src/main/java/com/fw/leetCode/KFrequenceElements.java
@@ -4,7 +4,15 @@ import java.util.*;
 import java.util.logging.Logger;
 
 /**
- * Created by fengwei on 2016/8/30.
+ * Leetcode 347. K Frequence Elements
+ *
+ * Given a non-empty array of integers, return the k most frequence elements.
+ * For Example:
+ * Given: [2,3,3,2,1,2,2] and k = 2, return [2, 3]
+ * @author fw
+ * Summary : Since the Algorithm's time complexity must be better than O(nlogn),
+ * traverse all array need n complexity, maintain heap invariant need logn
+ * use priority queue to solve problem.
  */
 public class KFrequenceElements {
 
@@ -13,19 +21,17 @@ public class KFrequenceElements {
     public List<Integer> topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], map.containsKey(nums[0]) ? map.get(nums[0]) + 1 : 1);
+            map.put(nums[i], map.containsKey(nums[i]) ? map.get(nums[i]) + 1 : 1);
         }
-
         Comparator<Map.Entry<Integer, Integer>> comparator = new Comparator<Map.Entry<Integer, Integer>>() {
             @Override
             public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
-                return o1.getValue() <= o2.getValue() ? 0 : 1;
+                int result =o2.getValue() - o1.getValue();
+                return result;
             }
         };
-
-        Queue<Map.Entry<Integer, Integer>> queue = new PriorityQueue(k, comparator);
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue(k, comparator);
         queue.addAll(map.entrySet());
-
         ArrayList<Integer> result = new ArrayList<Integer>();
         for (int i = 0; i < k; i++) {
             Map.Entry<Integer, Integer> entry = queue.poll();
@@ -36,15 +42,10 @@ public class KFrequenceElements {
     }
 
     public static void main(String[] args) {
-/*        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        map.put(2, 5);
-        map.put(2, 2);
-        log.info(map.size()+" ");
-        log.info(map.get(2)+"");*/
         KFrequenceElements kfe = new KFrequenceElements();
-        int[] temps = new int[]{2,3,2,3,3,4};
+        int[] temps = new int[]{2,2,2,3,3,1,2,2,2,3,3,3,3,3,3,5,6,7,8,9,32,32,14,52};
         List<Integer> list = new ArrayList<Integer>();
-        list = kfe.topKFrequent(temps, 2);
+        list = kfe.topKFrequent(temps, 4);
         for (Integer i : list) {
             log.info(i+"");
         }
