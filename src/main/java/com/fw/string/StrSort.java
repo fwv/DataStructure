54d0c476c6bcf0bb8de11b3ae27795ebbe996a90
fwv <cqufengwei@icloud.com>
2016-12-19 16:21:16 +0800

    Changes to be committed: 	modified:   .idea/workspace.xml 	modified:   out/artifacts/DataStructureAndAlgorithm_jar/DataStructureAndAlgorithm.jar 	new file:   src/main/java/com/fw/string/StrSort.java
     Untracked files:
    	target/classes/com/fw/string/

---
+++ src/main/java/com/fw/string/StrSort.java
@@ -0,0 +1,60 @@
+package com.fw.string;
+
+import com.fw.Tools.LogUtils;
+
+/**
+ * @Author fengwei
+ * Created on 2016/12/19/0019.
+ */
+public class StrSort {
+
+    //every string which in a[] must have same length.
+    public static void LSD(String[] a) {
+        int R = 256;
+        int N = a.length;
+        String [] aux = new String [N];
+        int W = a[0].length()-1;
+        for (int D = W; D >= 0; D--) {
+
+            int count[] = new int[R + 1];
+            // statistics frequency
+            for (int i = 0; i < N; i++) {
+                count[a[i].charAt(D) + 1]++;
+            }
+
+            // transform frequency to index
+            for (int m = 0; m < count.length-1; m++) {
+                count[m+1] += count[m];
+            }
+
+            // category
+            for (int j = 0; j < N; j++) {
+                aux[count[a[j].charAt(D)]++] = a[j];
+            }
+
+            // write back
+            for (int k = 0; k < N; k++) {
+                a[k] = aux[k];
+            }
+        }
+
+    }
+
+//    public static void MSD(String[] a) {
+//        int N = a.length;
+//        int R = 256;
+//        for () {
+//
+//        }
+//
+//    }
+
+    public static void main(String[] args) {
+        String [] strs = {"aldol", "casld","zzzzz","abcde","alsol"};
+        StrSort.LSD(strs);
+        for (int i = 0; i < strs.length; i++) {
+            LogUtils.log.info(strs[i]);
+        }
+    }
+
+}
